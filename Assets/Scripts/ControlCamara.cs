5d167c1ce242d125f474392e5595612c7c7e6f83
Sebpantoja1 <67817903+Sebpantoja1@users.noreply.github.com>
2025-05-21 16:57:04 -0500

    update alarma de gases

--- Assets/Scripts/ControlCamara.cs
+++ Assets/Scripts/ControlCamara.cs
@@ -2,16 +2,29 @@ using UnityEngine;
 
 public class ControlCamara : MonoBehaviour
 {
-    public bool camaraBloqueada = false;
     public float sensibilidad = 2f;
-    public Transform jugador;
+    public Transform cuerpoJugador;
+
+    float rotacionX = 0f;
+    public bool camaraBloqueada = false;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
     void Update()
     {
         if (camaraBloqueada) return;
 
         float mouseX = Input.GetAxis("Mouse X") * sensibilidad;
-        jugador.Rotate(Vector3.up * mouseX);
+        float mouseY = Input.GetAxis("Mouse Y") * sensibilidad;
+
+        rotacionX -= mouseY;
+        rotacionX = Mathf.Clamp(rotacionX, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(rotacionX, 0f, 0f);
+        cuerpoJugador.Rotate(Vector3.up * mouseX);
     }
 }
-
